6d208ed86b6de4aedd633564de3dfbb1f5281e69
Alexandre Feio <alefeio@gmail.com>
2025-01-06 15:25:43 -0300

    ajust layout 2 - ajust

--- src/context/AgendamentoContext.tsx
+++ src/context/AgendamentoContext.tsx
@@ -3,61 +3,61 @@ import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 // Definindo o tipo para os dados de agendamento
 interface AgendamentoData {
-  dadosPessoais: {
-    nome: string;
-    email: string;
-    cpf: string;
-    telefone: string;
-    endereco: string;
-  };
-  especialidade: {
-    id: string;
-    nome: string;
-  };
-  convenio: {
-    id: string;
-    nome: string;
-  };
-  medico: {
-    id: string;
-    nome: string;
-  } | null;
-  dataAgendamento: {
-    data: string;
-    horario: string;
-  }
+    dadosPessoais: {
+        nome: string;
+        email: string;
+        cpf: string;
+        telefone: string;
+        endereco: string;
+    };
+    especialidade: {
+        id: string;
+        nome: string;
+    };
+    convenio: {
+        id: string;
+        nome: string;
+    };
+    medico: {
+        id: string;
+        nome: string;
+    } | null;
+    dataAgendamento: {
+        data: string;
+        horario: string;
+    }
 }
 
 // Criando o contexto com um valor inicial vazio
 const AgendamentoContext = createContext<{
-  agendamentoData: AgendamentoData;
-  setAgendamentoData: React.Dispatch<React.SetStateAction<AgendamentoData>>;
+    agendamentoData: AgendamentoData;
+    setAgendamentoData: React.Dispatch<React.SetStateAction<AgendamentoData>>;
 }>({
-  agendamentoData: {
-    dadosPessoais: { nome: '', email: '', cpf: '', telefone: '', endereco: '' },
-    especialidade: { id: '', nome: ''},
-    convenio: { id: '', nome: ''},
-    medico: null,
-    dataAgendamento: '',
-  },
-  setAgendamentoData: () => {},
+    agendamentoData: {
+        dadosPessoais: { nome: '', email: '', cpf: '', telefone: '', endereco: '' },
+        especialidade: { id: '', nome: '' },
+        convenio: { id: '', nome: '' },
+        medico: null,
+        dataAgendamento: { data: '', horario: '' },
+    },
+    setAgendamentoData: () => { },
 });
 
 // Criando o provedor do contexto
 export const AgendamentoProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [agendamentoData, setAgendamentoData] = useState<AgendamentoData>({
-    dadosPessoais: { nome: '', email: '', cpf: '', telefone: '', endereco: '' },
-    especialidade: { id: '', nome: ''},
-    convenio: { id: '', nome: '' },
-    medico: null,
-    dataAgendamento: '',
-  });
+    const [agendamentoData, setAgendamentoData] = useState<AgendamentoData>({
+        dadosPessoais: { nome: '', email: '', cpf: '', telefone: '', endereco: '' },
+        especialidade: { id: '', nome: '' },
+        convenio: { id: '', nome: '' },
+        medico: null,
+        dataAgendamento: { data: '', horario: '' },
+    });
 
-  return (
-    <AgendamentoContext.Provider value={{ agendamentoData, setAgendamentoData }}>
-      {children}
-    </AgendamentoContext.Provider>
-  );
+    return (
+        <AgendamentoContext.Provider value={{ agendamentoData, setAgendamentoData }}>
+            {children}
+        </AgendamentoContext.Provider>
+    );
 };
 
 // Hook para acessar o contexto em outros componentes
