95fa8f02e0a4e41d91139cdcf46a3ca77d00fdeb
alefeio@gmail.com <alefeio@gmail.com>
2025-02-06 17:35:14 -0300

    ajust build 3

--- src/api.ts
+++ src/api.ts
@@ -6,13 +6,11 @@ const api = axios.create({
 
 api.interceptors.request.use(async (config) => {
     if (!config.headers.Authorization) {
-        const response = await axios.post('/api/token', {
-            username: '1cce8244-ee5e-477c-a0fb-64186980ef6d',
-            password: 'api@versatilis',
-            grant_type: 'password',
-        }, {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        const response = await axios.get('http://177.159.112.242:9091/versatilis/Token', {
+            headers: { 'Content-Type': 'text/plain' },
+            data: 'username=1cce8244-ee5e-477c-a0fb-64186980ef6d&password=api@versatilis&grant_type=password',
         });
+
         const token = response.data.access_token;
         config.headers.Authorization = `Bearer ${token}`;
     }
