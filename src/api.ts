b284d8dbbf3ba0aaa377a89a78bed8307e0bdbaf
alefeio@gmail.com <alefeio@gmail.com>
2025-02-11 11:03:54 -0300

    agendamento ajust 10

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ const fetchToken = async (): Promise<string> => {
     const username = encodeURIComponent('1cce8244-ee5e-477c-a0fb-64186980ef6d');
     const password = encodeURIComponent('api@versatilis');
     const grantType = encodeURIComponent('password');
-    
+
     const url = `/api/versatilisToken?username=${username}&password=${password}&grant_type=${grantType}`;
 
     try {
@@ -13,8 +13,17 @@ const fetchToken = async (): Promise<string> => {
             },
         });
 
+        // Verificar se a resposta está correta
+        const contentType = response.headers.get('content-type');
         if (!response.ok) {
-            throw new Error(`Erro ao buscar o token: ${response.statusText}`);
+            throw new Error(`Erro HTTP: ${response.status} - ${response.statusText}`);
+        }
+
+        // Se o servidor está retornando HTML, capturar isso antes de processar como JSON
+        if (!contentType || !contentType.includes('application/json')) {
+            const text = await response.text();
+            console.error('Resposta inesperada:', text);
+            throw new Error('A resposta do servidor não é um JSON válido.');
         }
 
         const data = await response.json();
